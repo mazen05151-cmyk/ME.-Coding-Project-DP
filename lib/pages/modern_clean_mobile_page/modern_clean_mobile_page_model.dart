4a35e05e4b5e16e3cc49060b8e06515c97101f05
Mazoony <mazen05151@gmail.com>
2026-06-24 21:26:37 +0300

    PAGE

--- lib/pages/modern_clean_mobile_page/modern_clean_mobile_page_model.dart
+++ lib/pages/modern_clean_mobile_page/modern_clean_mobile_page_model.dart
@@ -1,28 +1,22 @@
-import '/components/request_card2_widget.dart';
-import '/flutter_flow/flutter_flow_google_map.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_google_map.dart';
+import '/components/request_card2_widget.dart';
 import 'modern_clean_mobile_page_widget.dart' show ModernCleanMobilePageWidget;
 import 'package:flutter/material.dart';
 
-class ModernCleanMobilePageModel
-    extends FlutterFlowModel<ModernCleanMobilePageWidget> {
-  ///  State fields for stateful widgets in this page.
+class ModernCleanMobilePageModel extends FlutterFlowModel<ModernCleanMobilePageWidget> {
+  /// Map State Fields
+  LatLng? mapGoogleMapsCenter;
+  GoogleMapController? mapGoogleMapsController;
 
-  // State field(s) for Map Google Map widget.
-  LatLng? mapGoogleMapsCenter1;
-  final mapGoogleMapsController1 = Completer<GoogleMapController>();
-  // State field(s) for Map Google Map widget.
-  LatLng? mapGoogleMapsCenter2;
-  final mapGoogleMapsController2 = Completer<GoogleMapController>();
-  // Model for RequestCard.
+  /// Request List Card State Models
   late RequestCard2Model requestCardModel1;
-  // Model for RequestCard.
   late RequestCard2Model requestCardModel2;
-  // Model for RequestCard.
   late RequestCard2Model requestCardModel3;
 
   @override
   void initState(BuildContext context) {
+    // Initialize child sub-models for standard layout compliance
     requestCardModel1 = createModel(context, () => RequestCard2Model());
     requestCardModel2 = createModel(context, () => RequestCard2Model());
     requestCardModel3 = createModel(context, () => RequestCard2Model());
@@ -30,6 +24,7 @@ class ModernCleanMobilePageModel
 
   @override
   void dispose() {
+    mapGoogleMapsController?.dispose();
     requestCardModel1.dispose();
     requestCardModel2.dispose();
     requestCardModel3.dispose();
